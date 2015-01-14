6c673cdc7dd1dc22ff38f135154e15dd881a023c
Branko Vukelic <branko@brankovukelic.com>
2015-01-14 18:37:46 +0100

    Made tests pass

--- tests/app.py
+++ tests/app.py
@@ -8,16 +8,12 @@ except ImportError:
 import bottle
 from webtest import TestApp
 
-from bottle_utils import ajax, csrf, flash
+from bottle_utils import ajax
 
 bottle.debug()
-bottle.BaseTemplate.defaults.update({
-    'csrf_tag': csrf.csrf_tag
-})
 
 app = bottle.default_app()
 app.config.update({str('csrf.secret'): 'foo'})
-app.install(flash.message_plugin)
 
 
 # Test handler
@@ -25,40 +21,6 @@ app.install(flash.message_plugin)
 @ajax.ajax_only
 def ajax_only_handler():
     return 'success'
-
-
-@app.get('/csrf_token')
-@csrf.csrf_token
-def form_view():
-    return bottle.template('<form method="POST">{{! csrf_tag() }}</form>')
-
-
-@app.post('/csrf_token')
-@csrf.csrf_protect
-def post_view():
-    return 'success'
-
-
-@app.get('/csrf_token_other')
-@csrf.csrf_token
-def other_form_view():
-    return bottle.template('<form method="POST">{{! csrf_tag() }}</form>')
-
-
-@app.post('/csrf_token_other')
-@csrf.csrf_protect
-def other_post_view():
-    return 'success other'
-
-
-@app.get('/message')
-def get_message():
-    return str(bottle.request.message)
-
-
-@app.post('/message')
-def set_message():
-    bottle.response.flash('Come on!')
 
 
 test_app = TestApp(app, cookiejar=CookieJar())
