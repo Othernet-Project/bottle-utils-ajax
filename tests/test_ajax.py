f5316859ee4b79b85b625dbe646a646e03ccbe53
Branko Vukelic <branko@brankovukelic.com>
2015-03-27 18:15:04 +0100

    Added tests for roca_view

--- tests/test_ajax.py
+++ tests/test_ajax.py
@@ -2,7 +2,7 @@
 test_ajax.py: Unit tests for ``bottle_utils.ajax`` module
 
 Bottle Utils
-2014 Outernet Inc <hello@outernet.is>
+2014-2015 Outernet Inc <hello@outernet.is>
 All rights reserved
 
 Licensed under BSD license. See ``LICENSE`` file in the source directory.
@@ -10,24 +10,28 @@ Licensed under BSD license. See ``LICENSE`` file in the source directory.
 
 from __future__ import unicode_literals
 
-import sys
 try:
     from unittest import mock
 except ImportError:
     import mock
 
-from bottle_utils.ajax import *
+import bottle_utils.ajax as mod
 
-PY2 = sys.version_info.major == 2
 MOD = 'bottle_utils.ajax.'
+
+
+def mock_handler(**kwargs):
+    handler = mock.Mock()
+    handler.__name__ = str('handler')
+    handler.return_value = kwargs
+    return handler
 
 
 @mock.patch(MOD + 'request')
 @mock.patch(MOD + 'abort')
 def test_ajax_only_aborts_non_ajax(abort, request):
-    handler = mock.Mock()
-    handler.__name__ = str('handler')
-    ajax_handler = ajax_only(handler)
+    handler = mock_handler()
+    ajax_handler = mod.ajax_only(handler)
     request.is_xhr = True
     ajax_handler()
     assert not abort.called
@@ -36,9 +40,61 @@ def test_ajax_only_aborts_non_ajax(abort, request):
     abort.assert_called_with(400)
 
 
+@mock.patch(MOD + 'request')
+@mock.patch(MOD + 'template')
+def test_roca_as_view_deco(template, request):
+    handler = mock_handler()
+    roca_handler = mod.roca_view('foo', 'bar')(handler)
+    request.is_xhr = False
+    roca_handler()
+    template.assert_called_once_with('foo')
+
+
+@mock.patch(MOD + 'request')
+@mock.patch(MOD + 'template')
+def test_roca_as_view_deco_xhr(template, request):
+    handler = mock_handler()
+    roca_handler = mod.roca_view('foo', 'bar')(handler)
+    request.is_xhr = True
+    roca_handler()
+    template.assert_called_once_with('bar')
+
+
+@mock.patch(MOD + 'request')
+@mock.patch(MOD + 'template')
+def test_roca_with_defaults(template, request):
+    handler =  mock_handler()
+    roca_handler = mod.roca_view('foo', 'bar', foo='bar')(handler)
+    request.is_xhr = False
+    roca_handler()
+    template.assert_called_once_with('foo', foo='bar')
+
+
+@mock.patch(MOD + 'request')
+@mock.patch(MOD + 'template')
+def test_roca_with_defaults_merged(template, request):
+    handler =  mock_handler(bar='baz')
+    roca_handler = mod.roca_view('foo', 'bar', foo='bar')(handler)
+    request.is_xhr = False
+    roca_handler()
+    template.assert_called_once_with('foo', foo='bar', bar='baz')
+
+
+@mock.patch(MOD + 'request')
+@mock.patch(MOD + 'template')
+def test_roca_custom_template_func(template, request):
+    handler = mock_handler()
+    faux_tpl = mock.Mock()
+    roca_handler = mod.roca_view('foo', 'bar', template_func=faux_tpl)(handler)
+    roca_handler()
+    assert faux_tpl.called
+    assert not template.called
+
+
 # Integration tests
 
 from app import test_app
+
 
 def test_normal_request_returns_400():
     res = test_app.get('/ajax_only', expect_errors=True)
